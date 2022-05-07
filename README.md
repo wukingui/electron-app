4c717fe1a8c65e60dde49dee46206b7c3bb31fd5
吴金桂 <onevking@gmail.com>
2022-05-07 10:40:59 +0800

    上传项目

---
+++ README.md
@@ -0,0 +1,20 @@
+# Electron打包桌面应用程序
+
+## install
+```
+npm install
+```
+
+## run
+```
+
+npm start
+```
+
+## build
+```
+
+npm run postinstall
+
+npm run build
+```
---
+++ main.js
@@ -0,0 +1,30 @@
+// main.js
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // 加载 index.html
+  mainWindow.loadFile('index.html')
+}
+
+// 部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// 当所有窗口都被关闭的时候退出程序
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
